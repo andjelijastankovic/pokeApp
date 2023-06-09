f56176ee2e27ba812ad24bd4d5cb77d02164f03d
andjelijastankovic <andjelijastankovic95@gmail.com>
2023-06-09 16:49:30 +0200

    route check

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
         <Navbar />
           <Routes>
             <Route path="" element={<Main />} exact/>
-            <Route path=":name" element={<Pokemon />} exact/>
+            <Route path="/:name" element={<Pokemon />} exact/>
           </Routes>
       </BrowserRouter>
   );
